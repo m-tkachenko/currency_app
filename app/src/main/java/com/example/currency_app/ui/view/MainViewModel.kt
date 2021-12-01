63c8b9b7894ba4510d2157dfefb7eabb4c218f7a
Mykhailo Tkachenko <tk.mykhailo@gmail.com>
2021-12-01 14:10:32 +0100

    ViewModel Updated

--- app/src/main/java/com/example/currency_app/ui/view/MainViewModel.kt
+++ app/src/main/java/com/example/currency_app/ui/view/MainViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.currency_app.ui.view
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.currency_app.data.Currency
+import com.example.currency_app.data.Repository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel : ViewModel(){
+class MainViewModel(private val repository: Repository) : ViewModel(){
+    val dataList = MutableLiveData<Currency>()
+    val error = MutableLiveData<String>()
 
+    fun getData() {
+        val response = repository.getData()
+
+        response.enqueue(object : Callback<Currency> {
+            override fun onResponse(call: Call<Currency>, response: Response<Currency>) {
+                dataList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<Currency>, t: Throwable) {
+                error.postValue(t.message)
+            }
+
+        })
+    }
 }
